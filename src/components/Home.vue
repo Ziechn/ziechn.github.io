8e7a30189e52b49c7bbce509b166d0613e3c873e
ziechn <zedziechn@gmail.com>
2024-07-23 10:44:34 -0400

    Testing yet another location.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -17,14 +17,14 @@
     data() {
       return {
         hideNav: true,
-        imageSource: 'src/assets/images/chris-000.jpg',
+        imageSource: `@/assets/images/chris-000.jpg`,
       }
     },
     created() {
       const delayInSeconds = 4;
       let index = 0;
   
-      const imageDir = 'src/assets/images/';
+      const imageDir = `@/assets/images/`;
       const images = ['chris-000.jpg',
         'chris-001.jpg',
         'chris-002.jpg',
