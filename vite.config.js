9819714a528be458815601ba3569dd1375648a98
ziechn <zedziechn@gmail.com>
2024-07-31 10:49:19 -0400

    Correcting the error on the base url.

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/',
+  base: '/ziechn.github.io/',
   define: {
     'process.env.BASE_URL': JSON.stringify('/')
   }
