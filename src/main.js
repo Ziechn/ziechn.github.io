8f3929648135b9a5509a1ee162f5b802752ff7ae
ziechn <zedziechn@gmail.com>
2024-07-31 10:04:59 -0400

    More updates to suppor the router redirects.

--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import { createRouter } from './router'
+import router from './router'
 
-const router = createRouter();
 const app = createApp(App);
-
 app.use(router);
 app.mount('#app');
