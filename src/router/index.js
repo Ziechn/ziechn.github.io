b81fae069c0558d9db96a07f5525ba77dbd28559
ziechn <zedziechn@gmail.com>
2024-07-31 11:43:38 -0400

    Small adjustment to the base url.

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter as _createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 
 import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue';
@@ -22,8 +22,8 @@ const routes = [
   },
 ];
 
-const router = _createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL || '/'),
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes
 });
 
