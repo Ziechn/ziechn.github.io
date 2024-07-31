04843d3bc7d6dd838f6581f4ca58922b40e92351
ziechn <zedziechn@gmail.com>
2024-07-31 10:54:31 -0400

    Updating the base redirect.

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,5 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/ziechn.github.io/',
-  define: {
-    'process.env.BASE_URL': JSON.stringify('/')
-  }
+  base: '/ziechn.github.io/'
 })
