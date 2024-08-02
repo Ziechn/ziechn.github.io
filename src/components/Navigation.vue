e385456114711a8899a8458d63762afd2330d18a
ziechn <zedziechn@gmail.com>
2024-08-02 17:10:25 -0400

    Mobile navigation styling.

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -2,19 +2,29 @@
   <div class="navigation">
     <nav>
       <span class="name">Chris Cook's Portfolio</span>
-      <router-link v-bind:to="{ name: 'projects' }"><div>Projects</div></router-link>
-      <router-link v-bind:to="{ name: 'resume' }"><div>Resume</div></router-link>
-      <router-link v-bind:to="{ name: 'about' }"><div>About</div></router-link>
+      <router-link v-bind:to="{ name: 'projects' }" v-if="route.name != 'projects'">
+        <div>Projects</div>
+      </router-link>
+      <router-link v-bind:to="{ name: 'resume' }" v-if="route.name != 'resume'">
+        <div>Resume</div>
+      </router-link>
+      <router-link v-bind:to="{ name: 'about' }" v-if="route.name != 'about'">
+        <div>About</div>
+      </router-link>
     </nav>
   </div>
 </template>
 
 <script>
-export default {
+import { useRoute } from 'vue-router';
 
+
+export default {
+  setup() {
+    const route = useRoute();
+    return { route };
+  }
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
